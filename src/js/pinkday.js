ece3623c6c5e7f8fb8728924b35b5f43e5fa6db2
thronestudio <contact@throne.work>
2023-04-23 10:24:36 -0700

    revisions

--- src/js/pinkday.js
+++ src/js/pinkday.js
@@ -1,31 +1,47 @@
 (function($) {
 
-    $(document).ready(function() {
+    // play video
+    $("#player").ready(function() {
       $('#player').play();
+
+
+
     });
 
+    $(document).on('ready', function() {
 
+    });
+
+    function fadeInLogo() {
+      gsap.to('.logo', {
+        opacity: 1,
+        duration: 0.4,
+        delay: 1
+      });
+    }
+    fadeInLogo();
+
+    function fadeInEnter() {
+      gsap.to('.enter-button', {
+        opacity: 1,
+        duration: 0.3,
+        bottom: '80px',
+        delay: 1.8
+      });
+    }
+    fadeInEnter();
+
+    // hide
     $('.enter-button > span').on('click', function() {
         $('.enter').hide();
     });
 
-    // function cartSlideOut() {
-
-    //   $('.cart-drawer').addClass('active');
-
-    //   gsap.to('.cart-drawer', {
-    //     x: -400,
-    //   });
-
-    // }
-
+    // cart
     $('.cart-nav-opener').on('click', function() {
       $('body').addClass('js-my-cart-open');
     });
     $('.cart-drawer--close').on('click', function() {
       $('body').removeClass('js-my-cart-open');
     });
-
-
 
 })(jQuery);
